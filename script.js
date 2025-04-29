65de7342f1f0bca30eb4cbbc6f1551609633a921
SilvioKacorri <s.kacorri@calmcoders.net>
2025-04-29 18:00:33 +0200

    added resetGame function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let cards = ["Q","K","A","J","10","2","3","4","5","6","7","8","9"];
+let cards = ["Q","K","A","J","10","2","3","4","5","6","7","8","9","Q","K","A","J","10","2","3","4","5","6","7","8","9","Q","K","A","J","10","2","3","4","5","6","7","8","9","Q","K","A","J","10","2","3","4","5","6","7","8","9","Q","K","A","J","10","2","3","4","5","6","7","8","9","Q","K","A","J","10","2","3","4","5","6","7","8","9","Q","K","A","J","10","2","3","4","5","6","7","8","9","Q","K","A","J","10","2","3","4","5","6","7","8","9"];
 
 let dealerHand = document.getElementById("dealer-hand");
 let playerHand = document.getElementById("player-hand");
@@ -18,11 +18,17 @@ function randomCard(){
 let dealerCards = [];
 let playerCards = [];
 
-for (i = 0 ; i < 2; i++){
-    dealerCards.push(randomCard());
-    let tempInt = randomCard();
-    playerCards.push(tempInt);
+
+function dealHand() {
+    for (i = 0 ; i < 2; i++){
+        dealerCards.push(randomCard());
+        let tempInt = randomCard();
+        playerCards.push(tempInt);
+    }
 }
+
+dealHand();
+
 console.log(playerCards);
 console.log(dealerCards);
 
@@ -45,13 +51,28 @@ function changePlayerCards(){
     }
 }
 
+// function changeChards(user){
+//     user.innerHTML = '';
+//     for (let i in user){
+//         let img = document.createElement("img");    nuk mund ta ndryshoj parametrin si funksion , sepse DOM
+//         img.src = `${user[i]}_spades.png`;
+//         img.classList.add("cards");
+//         user.append(img);
+//     }
+// }
+
 changeDealerCards();
 changePlayerCards();
 
+let flagA = true;
+
 function evaluateCards(card){
     for (let i in cards){
-        if (card == "A"){
+        if (card == "A" && flagA == true){
             return 11;
+        }
+        if (card == "A" && flagA == false){
+            return 1;
         }
         if (card == "J"){
             return 10;
@@ -68,31 +89,102 @@ function evaluateCards(card){
     }
 };
 
-
-
-
-let player1Display = document.getElementById("player-hand-number");
-function calculatePlayer1Hand(){
+function evaluateHandCount(cards){
     let count = 0;
-    let tempInt;
-    for (let i in playerCards){
-        console.log(evaluateCards(playerCards[i]));
-        count = parseInt(count + evaluateCards(playerCards[i]));
+    for (let card in cards){
+        count = parseInt(count + evaluateCards(cards[card]));
     }
-    if (count >= 12) {
-        if (playerCards.includes("A")) {
-            console.log("1")
+    if (count > 21 && cards.includes("A")) {
+        count = 0;
+        flagA = false;
+        for (let card2 in cards){
+            count = parseInt(count + evaluateCards(cards[card2]));
         }
+        return count;
     }
-    player1Display.textContent = count;
+    return count;
 };
+
+// letrat llogariten sapo fillon loja 
+let storePlayerCount = 0;
+let playerBust = false;
+let dealerBust = false;
+
+let player1Display = document.getElementById("player-hand-number"); 
+player1Display.textContent = evaluateHandCount(playerCards);
+storePlayerCount = player1Display.textContent;
+
+function resetGame() {
+    if (storePlayerCount >= 21 || playerBust || dealerBust) {
+        playerCards = [];
+        dealerCards = [];
+        dealHand();
+        changeDealerCards();
+        changePlayerCards();
+        player1Display.textContent = evaluateHandCount(playerCards);
+    }
+}
+
+
+if (storePlayerCount == 21){
+    setTimeout ( () => 
+    alert("blackjack") , 800)
+}
+
+function hit(user){
+    user.push(randomCard());
+    let count = evaluateHandCount(user);
+    
+}
+
 
 function hit(){
     playerCards.push(randomCard());    
     console.log(playerCards);
+    let count = evaluateHandCount (playerCards);
     changePlayerCards();
-    calculatePlayer1Hand();
+    player1Display.textContent = count;
+    storePlayerCount = count;
+    if ( count > 21 ) { 
+        playerBust = true;
+        setTimeout( () => alert("BUST") , 100 ) 
+    }
+    resetGame();   
 }
 
-calculatePlayer1Hand();
+function hitDealer(){
+    dealerCards.push(randomCard());
+    changeDealerCards();
+    resetGame();
+}
+
+
+let dealerDisplay = document.getElementById("dealer-hand-number");
+
+function dealerTurn() {
+    let count = evaluateHandCount(dealerCards);
+
+    console.log(storePlayerCount);
+
+    if (count < 16 || count < storePlayerCount){
+        hitDealer();
+        dealerDisplay.textContent = count;
+
+        setTimeout(dealerTurn, 500);
+    }
+    else {
+        dealerDisplay.textContent = count;
+    }
+}
+
+function stand() {
+    let count = evaluateHandCount(dealerCards);
+    dealerDisplay.textContent = count;
+
+    dealerTurn();
+}
+
+
+
+// calculatePlayer1Hand();
 
